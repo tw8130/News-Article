47eb720f827fca27b55e6ed6d5774e3f60ab93b1
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-25 21:51:16 +0300

    create a class articles to define articles objects initiated by self.() method

--- app/models.py
+++ app/models.py
@@ -2,8 +2,22 @@ class Source:
     '''
     Source class to define Source Objects
     '''
-    def __init__(self,id,name,description):
+    def __init__(self,id,name,description,url,category):
         self.id = id
         self.name = name
         self.description = description
+        self.url = url
         self.category = category
+    
+class Articles:
+    '''
+    Article class to define Articles objects
+    '''
+    def __init__(self,source,author,title,description,urlToImage,url,publishedAt):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.url = url
+        self.publishedAt = publishedAt
