935721efdd9c875a20e97953b03ff94bbdde21f5
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-25 21:11:05 +0300

    add test case to test the behaviour of source class

---
+++ app/tests/source_test.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test to test the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source('bbc-news','BBC News','Use BBC News for up-to-the-minute news, breaking news, video, audio and feature stories')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+    
+
+if__name__== '__main__':
+    unittest.main()
