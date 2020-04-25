588d80ed9d56e12387f482daca5ce324a98e3ddf
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-25 22:03:44 +0300

    add testcase to check if source class is initialized

--- app/tests/source_test.py
+++ app/tests/source_test.py
@@ -10,11 +10,22 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source('bbc-news','BBC News','Use BBC News for up-to-the-minute news, breaking news, video, audio and feature stories','general')
+        self.new_source = Source('bbc-news','BBC News','Use BBC News for up-to-the-minute news, breaking news, video, audio and feature stories','"http://www.bbc.co.uk/news"','general')
 
     def test_instance(self):
+        '''
+        Test case to check if self.news_source is an instance of Source
+        '''
         self.assertTrue(isinstance(self.new_source,Source))
+
+    def test_init(self):
+        '''
+        Test case to check if source class is initialized
+        '''
+        self.assertEqual(self.new_source.id, 'bbc-news')
+        self.assertEqual(self.new_source.name,'BBC News')
+        self.assertEqual(self.new_source.description,'Use BBC News for up-to-the-minute news, breaking news, video, audio and feature stories')
     
 
 if__name__== '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
