5aa7e4ec222688f96b62bda64c84540f8bc786a2
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-26 21:24:58 +0300

    import os module to allow us interact with operating dependent fuctionality and use os.environ.get() to get news api key which is set as environment variables

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
    NEWS_API_BASE_URL='https://newsapi.org/v2/sources?api_key={}'
    NEWS_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+   NEWS_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
 
 
 
@@ -27,3 +30,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
