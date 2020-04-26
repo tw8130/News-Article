d39879b87b146d563add8f6ea9da0e3b62d204aa
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-26 21:49:17 +0300

    git commit -m"import flask class,bootstrap class from flask_extension,create bootstrap instance and used create_app() to take the configuration settting key as argument

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+# from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
+
+
+
+
+
+
+
+
+
+
+
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
+
+
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+# # Initializing Flask Extensions
+# bootstrap = Bootstrap(app)
+
+# from app import views
+# from app import error
---
+++ app/request.py
@@ -0,0 +1,6 @@
+import urllib.request,json
+from .models import Source,Article
+
+# Source = source.Source
+
+
