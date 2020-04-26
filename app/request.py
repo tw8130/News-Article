67f78e5a229d02e642726f94a5beb1dda105e816
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-26 22:22:41 +0300

    reate get_movies function,use format method on base url and pass api key then greate get_sources_url as final url for api request then > use the urllib.request.urllopen() that takes get_sources_url as an argument then use read function to read response and store it in get_sources_data variable

--- app/request.py
+++ app/request.py
@@ -13,3 +13,25 @@ def configure_request(app):
     base_url_articles=app.config["NEWS_API_ARTICLE_URL"]
 
 
+def get_sources():
+
+    """
+    Function that gets the json response to our url request
+    """
+
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
+
+
+
