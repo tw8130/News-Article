7c7954a43c2013b9a980166f16f02536a40019af
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-26 22:02:34 +0300

    import source ,article class from models.py file then set api-key,base url&base article url to none,use configure_request()function that takes application instance to replace none variables to application configuration objects

--- app/request.py
+++ app/request.py
@@ -2,5 +2,14 @@ import urllib.request,json
 from .models import Source,Article
 
 # Source = source.Source
+api_key = None
+base_url = None
+base_url_articles=None
+
+def configure_request(app):
+    global api_key,base_url,base_url_articles
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config["NEWS_API_BASE_URL"]
+    base_url_articles=app.config["NEWS_API_ARTICLE_URL"]
 
 
