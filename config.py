fc314626bd8b35d1cda4897136aea34dac04a368
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-26 21:14:19 +0300

    store the news article url in the config file

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ class Config:
     '''
     General configuration parent class
     '''
-   NEWS_API_BASE_URL='https://newsapi.org/v2/sources/{}?api_key={}'
+   NEWS_API_BASE_URL='https://newsapi.org/v2/sources?api_key={}'
+   NEWS_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
 
 
 
