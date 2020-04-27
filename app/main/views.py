cca330fc42f6794ed156886267d77c076006c29d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-27 13:12:14 +0300

    create an article object by calling get_articles() function and pass dynamic url source id then pass that route into our template file

--- app/main/views.py
+++ app/main/views.py
@@ -14,4 +14,14 @@ def index():
     news_sources = get_sources('sources')
     return render_template('index.html', title=title, sources=news_sources)
 
+@main.route('/articles/<source_id>')
+def articles(source_id):
+    '''
+    View source page function that returns a source page and its data
+    '''
+    title = f"{source_id} page"
+    #title = "Hello"
+    articles = get_articles(source_id)
+    return render_template('articles.html',title = title, articles = articles)
+
 
