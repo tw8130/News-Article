40335933683ae64286140ef365bcd9d4b9fa294c
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-27 13:02:44 +0300

    import get_articles function from request module and import request function from flask

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request
 from . import main
-from ..requests import get_sources
+from ..requests import get_sources,get_articles
 
 #Views
 @main.route('/')
@@ -8,8 +8,10 @@ def index():
     '''
     View root page function that returns index page and the various news sources
     '''
-    
+
     title = 'Home- Welcome News Highlights Website'
     # Getting the news sources
     news_sources = get_sources('sources')
     return render_template('index.html', title=title, sources=news_sources)
+
+
