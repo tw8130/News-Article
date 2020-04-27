a5f7f53ccd845f6bf9af9e76a9acb418e8c05d53
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-27 11:35:37 +0300

    define our route decotator using main blueprint instance instead of app instance to define view function index then create variable news_sources to call get_sources function then pass result from function call to our template

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,15 @@
 from flask import render_template
 from . import main
 from ..requests import get_sources
+
+#Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns index page and the various news sources
+    '''
+    
+    title = 'Home- Welcome News Highlights Website'
+    # Getting the news sources
+    news_sources = get_sources('sources')
+    return render_template('index.html', title=title, sources=news_sources)
