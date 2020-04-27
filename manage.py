99b896d7921d36159c889b0d719fec0ac502b414
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-27 17:25:30 +0300

    call create_app function then instantiate manager class by passing app instance then use add_command to create new command server to launch the server

--- manage.py
+++ manage.py
@@ -1,2 +1,8 @@
 from app import create_app
 from flask_script import Manager,Server
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
